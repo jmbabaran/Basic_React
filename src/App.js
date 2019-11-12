f7ac332a1620b397da447dc472ded8d92eea7f1b
Jen <jmbabaran@gmail.com>
2019-11-12 19:18:37 +1100

    Uploading Basic React app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="shopping-list">
+    <h1>Shopping List for Online Media</h1>
+    <ul>
+      <li>Instagram</li>
+      <li>WhatsApp</li>
+      <li>Oculus</li>
+    </ul>
+  </div>
+  );
+}
+
+export default App;
